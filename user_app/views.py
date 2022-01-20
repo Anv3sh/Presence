24e2ac2a7040517ebaff70af6738fa96355c19ed
Anv3sh <anveshgreat11@gmail.com>
2022-01-21 00:01:17 +0530

    changed email to username

--- user_app/views.py
+++ user_app/views.py
@@ -5,9 +5,9 @@ from django.contrib.auth.models import User
 # Create your views here.
 def loginpage(request):
     if request.method=='POST':
-        email=request.POST['email']
+        username=request.POST['username']
         password=request.POST['password']
-        user = authenticate(request, email=email, password=password)
+        user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request,user)
     return render(request,"user_app/index.html")
